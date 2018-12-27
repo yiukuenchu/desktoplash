0c4643a097f8fa385d6a39e2432936a10f9a345d
yiukuenchu <yiukuenchu@gmail.com>
2018-12-27 22:33:01 +0800

    fix

--- background.js
+++ background.js
@@ -1,10 +1,11 @@
-{
-    "name": "Getting Started Example",
-    "version": "1.0",
-    "description": "Build an Extension!",
-    "background": {
-      "scripts": ["background.js"],
-      "persistent": false
-    },
-    "manifest_version": 2
-}
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+'use strict';
+
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.storage.sync.set({color: '#3aa757'}, function() {
+    console.log("The color is green.");
+  });
+});
