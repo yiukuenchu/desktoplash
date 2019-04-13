1a8e848f61923975bfe3d1154069a51d8a7c5352
yiukuenchu <yiukuenchu@gmail.com>
2019-04-13 13:55:36 +0800

    加入 loading 效果

---
+++ popup.js
@@ -0,0 +1,7 @@
+window.onload = function(){
+    setTimeout(function(){
+    var loader = document.getElementsByClassName("loader")[0];
+    loader.className="loader fadeout" ;//使用渐隐的方法淡出loading page
+    setTimeout(function(){loader.style.display="none"},1000)
+    },1000)//强制显示loading page 1s
+}
