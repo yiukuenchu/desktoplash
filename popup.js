0b136bd40cb0ccbefb0ef11f380c6376473dce79
yiukuenchu <yiukuenchu@gmail.com>
2019-07-08 19:53:46 +0800

    coding on searchbox

--- popup.js
+++ popup.js
@@ -1,7 +1,14 @@
-window.onload = function(){
-    setTimeout(function(){
-    var loader = document.getElementsByClassName("loader")[0];
-    loader.className="loader fadeout" ;//使用渐隐的方法淡出loading page
-    setTimeout(function(){loader.style.display="none"},1000)
-    },1000)//强制显示loading page 1s
+// 图片淡出效果，但还未能实现
+// window.onload = function(){
+//     setTimeout(function(){
+//     var loader = document.getElementsByClassName("loader")[0];
+//     loader.className="loader fadeout" ;//使用渐隐的方法淡出loading page
+//     setTimeout(function(){loader.style.display="none"},1000)
+//     },1000)//强制显示loading page 1s
+// }
+
+function searchKey(key){
+    this.key = key;
 }
+
+const key
